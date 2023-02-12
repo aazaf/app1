e0d788ae95eb891ba081a2a748c0e118fd1d80b1
aazaf <aazafmax2@gmail.com>
2023-02-12 10:07:00 +0530

    customer form handling with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Customer {
+  name: string;
+  address: string;
+  salary: number;
+  registerDate: Date;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app1';
+
+  customers: Customer [] = [];
+
+  name = '';
+  address = '';
+  salary = 0;
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setAddress(address: string) {
+    this.address = address;
+  }
+
+  setSalary(salary: string) {
+    this.salary = Number.parseInt(salary);
+  }
+
+  saveCustomer() {
+
+    this.customers.push({
+      'name': this.name,
+      'address': this.address,
+      'salary': this.salary,
+      'registerDate':new Date()
+    });
+    console.log(this.customers);
+  }
 }
